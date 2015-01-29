e80263c7a9beee1b2042aa6bd5ac0a2e6f5f142a
Rebeca <rebeca@cern.ch>
2015-01-29 18:04:44 +0100

    nicer

--- chain.C
+++ chain.C
@@ -2,9 +2,9 @@
 #include "TSystem.h"
 
 void chain(int nsel = 0, int mode = 0, bool silent = false){
-gROOT->LoadMacro("ttHanalysis.C");
+  gROOT->LoadMacro("ttHanalysis.C");
 
-ttHanalysis an(0);
-an.Loop();
+  ttHanalysis an(0);
+  an.Loop();
 
 }
--- ttHanalysis.C
+++ ttHanalysis.C
@@ -8,15 +8,15 @@ void ttHanalysis::Loop()
 {
 
 
-   //Long64_t nentries = fChain->GetEntriesFast();
+  //Long64_t nentries = fChain->GetEntriesFast();
 
   // Long64_t nbytes = 0, nb = 0;
-   //for (Long64_t jentry=0; jentry<nentries;jentry++) {
-    //  Long64_t ientry = LoadTree(jentry);
-      //if (ientry < 0) break;
-     // nb = fChain->GetEntry(jentry);   nbytes += nb;
-     //cout << "hey baby" << endl;
-      // if (Cut(ientry) < 0) continue;
-   //}
+  //for (Long64_t jentry=0; jentry<nentries;jentry++) {
+  //  Long64_t ientry = LoadTree(jentry);
+  //if (ientry < 0) break;
+  // nb = fChain->GetEntry(jentry);   nbytes += nb;
+  //cout << "hey baby" << endl;
+  // if (Cut(ientry) < 0) continue;
+  //}
 }
 
